20efd4aa701c1a33e8e9f975c278dbf7bf62ff0a
“Foster <fosterataylor@yahoo.com>
2020-05-04 15:49:59 -0600

    Add package.json info and refactor session.js

--- session.js
+++ session.js
@@ -1,9 +1,11 @@
 var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder().forBrowser('chrome').build();
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
-driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
-driver.wait(webdriver.until.elementLocated({ xpath: '//*[@id="rso"]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/div[2]/h3/a' }), 500000)
-driver.findElement({xpath: '//*[@id="rso"]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/div[2]/h3/a'}).click();
-driver.getTitle().then(function(title){console.log(title);});
-driver.quit()
+async function example() {
+    let driver = await new webdriver.Builder().forBrowser('chrome').build();
+    await driver.get('http://www.google.com');
+    await driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
+    await driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
+    await driver.findElement({ xpath: '//*[@id="rso"]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/div[2]/h3/a' }).click();
+    await driver.getTitle().then(function(title){console.log(title);});
+}
+example();
+
